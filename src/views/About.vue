3e59ae301c42b64a628e3f297bc6787e9d05c6fe
Sutaa0101 <sutaahbts@yahoo.co.jp>
2019-05-23 00:49:27 +0900

    [add]Aboutのページを追加

---
+++ src/views/About.vue
@@ -0,0 +1,55 @@
+<template>
+    <div id="about">
+        <p class="title">About</p>
+        <div class="content">
+            <p>本サイトを閲覧いただき、誠にありがとうございます。</p>
+
+            <br>
+
+            <p>サイト作成者：</p>
+            <ul>
+                <li>星 綾真</li>
+                <li>21歳。現在、埼玉県在住。</li>
+            </ul>
+
+            <br>
+            <p>高校卒業後、ゲーム系の専門学校へ進学するも、家庭の経済事情により中退。</p>
+            <p>しかし、趣味で行っていたサイト制作、並びにゲーム制作をフリーターで行いつつ継続し、此度Webコーダーとして転職を志す。</p>
+            <br>
+            <p>HTML5 / CSS3 / javaScript / TypeScript / jQuery / Vue.js の経験あり。</p>
+            <p>短期ではあるが、アルバイトでの実務経験もあります。</p>
+            <br>
+            <p>Microsoft Office Specialist Master / 色彩検定2級 の2つの資格を保持。</p>
+
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    #about{
+        height:calc(100vh - 23vh);
+        background:linear-gradient(
+        rgb(255,252,234),
+        rgb(247,242,210)
+        );
+        padding-top:23vh;
+
+        .title{
+            text-align: center;
+            font-size: 3em;
+            font-family: "Cambria";
+            margin-bottom:1em;
+            user-select: none;
+        }
+
+        .content{
+            p{
+                text-indent: 1em;
+            }
+            ul{
+                list-style-type: none;
+                margin:0;
+            }
+        }
+    }
+</style>
--- src/views/Works.vue
+++ src/views/Works.vue
@@ -1,7 +1,60 @@
 <template>
-    
+    <div id="works">
+        <p class="title">Works</p>
+        <div class="content">
+            <div class="siteList">
+                <a href="https://sutaa0101.github.io/home/" target="_blank">
+                    <img src="../assets/digi_site.png" alt="DIGITAL GAMES">
+                    </a>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+    @import "../style/boxShadow.scss";
 
+    #works{
+        height:calc(100vh - 23vh);
+        background:linear-gradient(
+        rgb(255,252,234),
+        rgb(247,242,210)
+        );
+        padding-top:23vh;
+
+        .title{
+            text-align: center;
+            font-size: 3em;
+            font-family: "Cambria";
+            margin-bottom:1em;
+            user-select: none;
+        }
+
+        .content{
+            p{
+                text-indent: 1em;
+            }
+            ul{
+                list-style-type: none;
+                margin:0;
+            }
+            img{
+                display:inline-block;
+                max-width:225px;
+                width:35%;
+                transition: 0.2s;
+                @include buttonShadow();
+                opacity:0.8;
+
+                &:first-child{
+                    margin-right:1em;
+                }
+                &:hover{
+                    opacity:1;
+                    @include boxShadow(4px,4px);
+                }
+
+            }
+        }
+    }
 </style>
